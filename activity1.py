80029a5274340d1395cc55948dbdb1af0d8c5f8f
sophc <sophiecheng181@gmail.com>
2025-11-23 12:00:10 -0500

    Activity 1 pushesd

---
+++ activity1.py
@@ -0,0 +1,20 @@
+class ShoppingCart:
+ def __init__(self):
+   self.cart = None #replace None with cart
+   self.prices = {"apple" : 1, "chicken" : 4, "broccoli" : 2, "baked beans" : 3}
+ 
+'''Adds {quantity} of {item} to the cart'''
+ def add(self, item, quantity):
+   return None
+
+ '''Deletes {item} from the cart'''
+ def delete(self, item):
+   return None
+
+ '''Returns the total cost of all the items in the cart'''
+ def total_cost(self):
+   total = 0
+   for __ in ___:
+     '''write body for the for loop '''
+   return total
+
